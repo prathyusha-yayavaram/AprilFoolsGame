60261d5896e585d5695cc893519e1f0dc7e41963
Jordan McWilliams <jmcwilli@usc.edu>
2023-03-31 22:09:53 -0700

    PlaceBlocks

---
+++ Assets/Scripts/ClickPlaceBlock.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ClickPlaceBlock : MonoBehaviour
+{
+    public Tilemap ground;
+    public Tile tileToPlace;
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            ground.SetTile(ground.WorldToCell(pos), tileToPlace);
+        }
+    }
+}
